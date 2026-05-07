798da9a5db7c0e0e76c7e61de05a96e1a4c556ac
Mustang <167328206+Mustang0111@users.noreply.github.com>
2026-05-07 15:50:40 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -343,4 +343,6 @@ if __name__ == '__main__':
     if not os.path.exists('database.db'):
         init_db()
 
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(host='0.0.0.0', port=port)
